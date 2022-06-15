ea539df24ea809bbb91fe71a50bba06566952526
Alessandro <alessandro.mello@codezone.com.br>
2022-06-15 14:14:55 -0300

    [Page-home] Section Qualifications

--- static/page-home.php
+++ static/page-home.php
@@ -29,12 +29,12 @@
     <div class="content-header aling-items-center">
         <div class="container h-100 pt-4">
             <div class="teste px-4">
-                <h3 class="title-h2 pt-3" >Quer sabe mais sobre nosso JOBS</h3>
-                <p class="subtitle pt-3" >Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos.</p>
-                <div class="inputJobs mt-5">
+                <!-- <h3 class="title-h2 pt-3" >Quer sabe mais sobre nosso JOBS</h3>
+                <p class="subtitle pt-3" >Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos.</p> -->
+                <!-- <div class="inputJobs mt-5">
                     <input type="text" placeholder="Digite aqui seu email" class="ms-3 text-light">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#fff" width="36px" height="36px"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/></svg>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
@@ -51,7 +51,7 @@
             </p>
         </div>
         <div class="content-footer">
-            <div class="container mt-5">
+            <div class="mt-5">
                 <div class="row justify-content-around">
                     <div class="col about-card bg-light mb-5">
                         <div class="h-50 d-flex align-items-center mx-3">
@@ -97,7 +97,7 @@
 </section>
 
 <section class="qualifications pt-5">
-    <div class="container pt-5">
+    <div class="container">
         <div class="content-header text-start">
             <h2 class="title text-dark mb-5">Qualificações</h2>
             <p class="subtitle-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi
@@ -108,12 +108,9 @@
                  laboriosam culpa quae magni. Expedita harum ducimus laudantium.
             </p>
         </div>
-        <div class="content-body">
-
-            <div class="container">
-                <div class="row justify-content-around">
-
-                <div class="qualifications-card">
+        <div class="content-body container">
+            <div class="row justify-content-around">
+                <div class="col qualifications-card">
                     <div>
                         <!-- <img src="" alt=""> -->
                         <div class="card-image bg-dark">
@@ -121,7 +118,7 @@
                         </div>
                     </div>
                     <div class="">
-                        <h3 class="fs-5 fw-bold text-dark mt-4">UX/UI DESIGNER</h3>
+                        <h3 class="fs-5 fw-bold text-dark mt-4 mb-5">UX/UI DESIGNER</h3>
                         <p class="subtitle-2 fs-6 mt-4">
                         Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de 
                         impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido
@@ -134,7 +131,7 @@
                     </div>
                 </div>
 
-                <div class="qualifications-card">
+                <div class="col qualifications-card">
                     <div>
                         <!-- <img src="" alt=""> -->
                         <div class="card-image bg-dark">
@@ -142,7 +139,7 @@
                         </div>
                     </div>
                     <div class="">
-                        <h3 class="fs-5 fw-bold text-dark mt-4">UX/UI DESIGNER</h3>
+                        <h3 class="fs-5 fw-bold text-dark mt-4 mb-5">UX/UI DESIGNER</h3>
                         <p class="subtitle-2 fs-6 mt-4">
                         Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de 
                         impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido
@@ -155,7 +152,7 @@
                     </div>
                 </div>
 
-                <div class="qualifications-card">
+                <div class="col qualifications-card">
                     <div>
                         <!-- <img src="" alt=""> -->
                         <div class="card-image bg-dark">
@@ -163,7 +160,7 @@
                         </div>
                     </div>
                     <div class="">
-                        <h3 class="fs-5 fw-bold text-dark mt-4">UX/UI DESIGNER</h3>
+                        <h3 class="fs-5 fw-bold text-dark mt-4 mb-5">UX/UI DESIGNER</h3>
                         <p class="subtitle-2 fs-6 mt-4">
                         Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de 
                         impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido
@@ -174,11 +171,8 @@
                         </p>
                         <a href="#" class="mt-5">SAIBA MAIS --></a>
                     </div>
-                </div>
-
                 </div>
             </div>
-
         </div>
 </section>
                 
